dae41386537b4ea8703b4652053d713cd01e756b
Sureshraja Singaravadivelu <ssingaravadivelu@adhslx.com>
2019-03-04 14:15:14 +0800

    Initial commit'

---
+++ src/main/java/com/genting/codetest/moneyexchange/MoneyexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.genting.codetest.moneyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyexchangeApplication.class, args);
+    }
+
+}
